5f592d951652e3e2626a2cdf7440bfeb02b275db
Tsukiyonocm <tsukiyonocm@yahoo.com>
2018-06-28 00:20:47 -0400

    basic setup done minus validation

--- js/scripts.js
+++ js/scripts.js
@@ -31,6 +31,58 @@ function saveBookmark(e){
         localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
     }
 
+    // Re-fetch Bookmarks
+    fetchBookmarks();
+
     //Prevent Form from Submitting
     e.preventDefault();
 };
+
+// Delete Bookmark
+function deleteBookmark(url){
+    // Get from localStorage
+    let bookmarks = JSON.parse(localStorage.getItem("bookmarks"));
+
+    // loop through bookmarks
+    for(let i = 0; i < bookmarks.length; i++){
+        if (bookmarks[i].url == url) {
+            //Remove from array
+            bookmarks.splice(i, 1);
+        }
+    }
+
+    // Re-set back to localStorage
+    localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
+
+    // Re-fetch Bookmarks
+    fetchBookmarks();
+}
+
+
+// Fetch Bookmarks
+function fetchBookmarks() {
+    // Get from localStorage
+    let bookmarks = JSON.parse(localStorage.getItem("bookmarks"));
+
+    // Get Output ID
+    let bookmarksResults = document.getElementById("bookmarksResults");
+    
+   
+    // Build Output
+    bookmarksResults.innerHTML = "";
+    
+    for(let i = 0; i < bookmarks.length; i++){
+        let name = bookmarks[i].name;
+        let url = bookmarks[i].url;
+
+        bookmarksResults.innerHTML += 
+        '<div class="bookmark">' + 
+            '<h3 class="bookmark-info">' + name +
+                '<div class="buttons">' + 
+                    '<a class="btn-primary" target="_blank" href="' + url + '"> Visit</a> ' +
+                    '<a onclick="deleteBookmark(\''+ url +'\')" class="btn-primary" href="#"> Delete</a> ' +
+                '</div>'
+            '</h3>' +
+        '</div>';
+    };
+}
